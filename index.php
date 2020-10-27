69008c9eafe6156e4f113949ab5661aafabc3e84
poafernandes <alexandre.poaf@gmail.com>
2020-10-27 18:20:47 -0300

    Enviado os arquivos

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Vacinas - PanVel Farmácias </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="page-container">
+        <div id="wrapper-header">
+            <header>
+                <div class="page-header">
+                    <!-- <div class="header-top-desktop"> -->
+                        <a href="#" class="logo"> <img src="content/panvel.svg"> </a>
+                        <a href="#"><span class="icon-menu"></span> CATEGORIAS</a>
+                        <a href="#" class="search"><input type="text" placeholder="O QUE VOCÊ PROCURA?"></a>
+                        <a href="#"><span class="icon-profile"></span> ENTRAR</a>
+                        <a href="#"><span class="icon-cart"></span></a>
+                    <!-- </div> --> 
+                </div>
+            </header>
+        </div>
+        <div id="wrapper-content"> 
+            <div class="content-container">
+                <div class="page-info">
+                    <div class="nav-tree">Página Inicial \ Clinic </div>
+                    <h2>Vacinas</h2>
+                </div>
+                <div id="destaque" class="content-row">
+                    <article>
+                        <h1> <span>Vacina</span> Hexavalente</h1>
+                        <span class="preco">R$656,90</span>
+                        <span class="link"><a href="#"> <span class="icon-calendar"></span> AGENDE SUA VACINA AQUI </a></span>
+                    </article>
+                </div>
+                <div class="content-row"> 
+                    <article class="preview">
+                        <div class="thumbnail">
+                            <figure><img src="content/Shape.png">
+                            </figure>
+                        </div>
+                        <div class="text">
+                            <div><h2> Sobre a Vacina </h2></div>
+                            <div><p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu condimentum arcu, vel luctus mi. Aenean interdum augue at quam consequat rutrum. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacus mauris, vestibulum in facilisis a, placerat non erat. </p></div>
+                            <div><h5> Sed imperdiet neque ante, ac vestibulum quam mollis sed: </h5></div>                        
+                        </div>
+                    </article>
+                </div>
+                <div class="content-row"> 
+                    <article class="preview">
+                        <div class="thumbnail">
+                            <figure><img src="content/Group_7.png">
+                            </figure>
+                        </div>
+                        <div class="text">
+                            <div><h2> Prevenção </h2></div>
+                            <div><p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu condimentum arcu, vel luctus mi. Aenean interdum augue at quam consequat rutrum. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacus mauris, vestibulum in facilisis a, placerat non erat. </p></div>
+                            <div><h5> Sed imperdiet neque ante, ac vestibulum quam mollis sed: </h5></div>                        
+                        </div>
+                    </article>
+                </div>
+                <div class="content-row"> 
+                    <article class="preview">
+                        <div class="thumbnail">
+                            <figure><img src="content/prescription(1).png">
+                            </figure>
+                        </div>
+                        <div class="text">
+                            <div><h2> Posologia </h2></div>
+                            <div><p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu condimentum arcu, vel luctus mi. Aenean interdum augue at quam consequat rutrum. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacus mauris, vestibulum in facilisis a, placerat non erat. </p></div>
+                            <div><h5> Sed imperdiet neque ante, ac vestibulum quam mollis sed: </h3></div>                        
+                        </div>
+                    </article>
+                </div>
+                <div class="content-row"> 
+                    <article class="preview">
+                        <div class="thumbnail">
+                            <figure><img src="content/Group_8.png">
+                            </figure>
+                        </div>
+                        <div class="text">
+                            <div><h2> Reações Adversas </h2></div>
+                            <div><p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu condimentum arcu, vel luctus mi. Aenean interdum augue at quam consequat rutrum. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacus mauris, vestibulum in facilisis a, placerat non erat. </p></div>
+                            <div><h5> Sed imperdiet neque ante, ac vestibulum quam mollis sed: </h5></div>                        
+                        </div>
+                    </article>
+                </div>
+                <div class="content-row"> 
+                    <article class="steps">
+                        <div class="steps-header"> <h2> Como Agendar sua Vacina na Panvel </h2> </div>
+                        <div class="steps-desc"><p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu condimentum arcu, vel luctus mi. Aenean interdum augue at quam consequat rutrum. </p></div>
+                        <div class="steps-gallery">
+                            <figure>
+                                <img src="content/Group_5.png">
+                                <figcaption>1.<br/>Lorem ipsum dolor sit amet</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="content/Group_5.png">
+                                <figcaption>2.<br/>Lorem ipsum dolor sit amet</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="content/Group_5.png">
+                                <figcaption>3.<br/>Lorem ipsum dolor sit amet</figcaption>
+                            </figure>
+                            <figure>
+                                <img src="content/Group_5.png">
+                                <figcaption> 4.<br/>Lorem ipsum dolor sit amet </figcaption>
+                            </figure>     
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+</html>
